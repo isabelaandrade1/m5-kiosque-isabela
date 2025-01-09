b1e979e7d7a66aef21232c8099abe0c8c9e76642
isabelaandrade1 <isabela.andrade.hey@gmail.com>
2025-01-09 11:01:37 -0400

    tarefa x finalizada

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from management.product_handler import get_product_by_id, get_products_by_type
+
 if __name__ == "__main__":
-    # Seus prints de teste aqui
-    ...
+    # Teste da função get_product_by_id
+    print(get_product_by_id(28))  # Deve exibir o produto com ID 28
+
+    # Teste da função get_products_by_type
+    print(get_products_by_type('drink'))  # Deve exibir todos os produtos do tipo 'drink'
